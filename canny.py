97ebd5dc51147e605a06853994a3c1dd415cc741
kipunaec <kipunaec@gmail.com>
2022-08-26 12:02:04 -0500

    Añadí comentarios al codigo

--- canny.py
+++ canny.py
@@ -1,10 +1,13 @@
+#Bibliotecas
 import cv2
 
+#abrir imagen
 img = cv2.imread('libro.jpg',0)
+#Aplicar Canny
 bordeCanny = cv2.Canny(img,100,200)
-
+#Mostrar imágenes
 cv2.imshow('Original', img)
 cv2.imshow('blur', bordeCanny)
-
+#Presionar una tecla para dejar de ejecutar
 cv2.waitKey(0)
 cv2.destroyAllWindows()
