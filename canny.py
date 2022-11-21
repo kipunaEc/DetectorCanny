02f46f70d8002695d63eb480979732792e25cdf4
kipunaec <kipunaec@gmail.com>
2022-11-21 10:56:01 -0500

    Corregir el nombre de la ventana Canny

--- canny.py
+++ canny.py
@@ -7,7 +7,7 @@ img = cv2.imread('libro.jpg',0)
 bordeCanny = cv2.Canny(img,100,200)
 #Mostrar imágenes
 cv2.imshow('Original', img)
-cv2.imshow('blur', bordeCanny)
+cv2.imshow('Canny', bordeCanny)
 #Presionar una tecla para dejar de ejecutar
 cv2.waitKey(0)
 cv2.destroyAllWindows()
